4d150ef6487f822d13befb0754ddb87744a059e8
Laurell Seville <laurellseville2000@gmail.com>
2021-06-25 02:11:45 -0500

    feat, update package.json

--- src/modules/subscription/components/steps/StepTwo.tsx
+++ src/modules/subscription/components/steps/StepTwo.tsx
@@ -1,5 +1,6 @@
 import React, { Dispatch, SetStateAction } from "react"
 import CreditCardInput from 'react-credit-card-input';
+import CardHeader from "../CardHeader";
 
 
 type Props = {
@@ -16,13 +17,7 @@ type Props = {
 const StepTwo: React.FC<Props> = ({ cardNumber, setCardNumber, setStep, cardExpirationDate, setCardExpirationDate, cardSecurityCode, setCardSecurityCode }) => {
     return (
         <div>
-            <div className="flex justify-center">
-                <h1 className="card-title text-center mr-2">we<span className="highlighted-text">store</span></h1>
-                <div className="flex items-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-database"><ellipse cx="12" cy="5" rx="9" ry="3"></ellipse><path d="M21 12c0 1.66-4 3-9 3s-9-1.34-9-3"></path><path d="M3 5v14c0 1.66 4 3 9 3s9-1.34 9-3V5"></path></svg>
-                </div>
-            </div>
-            <p className="card-subtitle text-center max-w-xs mx-auto">Please choose a plan that works for you and your team</p>
+            <CardHeader />
 
             <div className="fields my-10">
                 <CreditCardInput
